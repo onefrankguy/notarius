ce10174e9079272ea20979f4d229d785253dcf6d
elimossinary <me@frankmitchell.org>
2012-06-23 18:52:03 -0400

    Better documentation for Rake tasks.

--- rakefile.rb
+++ rakefile.rb
@@ -3,11 +3,13 @@ require 'rspec/core/rake_task'
 
 CLEAN.include 'coverage'
 
+desc 'Run specs.'
 task :default => :spec
 
+desc 'Run specs.'
 RSpec::Core::RakeTask.new :spec
 
-desc 'Generate SimpleCov test coverage'
+desc 'Generate SimpleCov spec coverage.'
 RSpec::Core::RakeTask.new :coverage do |t|
   t.rspec_opts = ['--require simplecov_helper']
 end
