8af91f383a87803bc7795ea7eb4c83e91c8430c6
elimossinary <me@frankmitchell.org>
2012-06-23 18:49:07 -0400

    Adding SimpleCov code coverage.

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,13 @@
+require 'rake/clean'
 require 'rspec/core/rake_task'
+
+CLEAN.include 'coverage'
 
 task :default => :spec
 
 RSpec::Core::RakeTask.new :spec
 
+desc 'Generate SimpleCov test coverage'
+RSpec::Core::RakeTask.new :coverage do |t|
+  t.rspec_opts = ['--require simplecov_helper']
+end
