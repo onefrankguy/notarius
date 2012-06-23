1237bf0220e51914a83b7837bf0c24a960b2a3c3
elimossinary <me@frankmitchell.org>
2012-06-23 19:37:59 -0400

    Adding a test to make sure namespace overwriting works.

--- spec/notarius_spec.rb
+++ spec/notarius_spec.rb
@@ -20,4 +20,32 @@ describe Notarius do
     Player.new
     File.read('player.log').should include('New player created!')
   end
+
+  it 'allows namespaces to be overwritten' do
+    FileUtils.rm 'player.log' if File.exists? 'player.log'
+    Notarius.configure 'BIG' do |l|
+      l.file.path = 'player.log'
+    end
+    class Player
+      include Notarius::BIG
+      def initialize
+        log.info 'New player created!'
+      end
+    end
+    Player.new
+    File.read('player.log').should include('New player created!')
+
+    FileUtils.rm 'monster.log' if File.exists? 'monster.log'
+    Notarius.configure 'BIG' do |l|
+      l.file.path = 'monster.log'
+    end
+    class Monster 
+      include Notarius::BIG
+      def initialize
+        log.info 'New monster created!'
+      end
+    end
+    Monster.new
+    File.read('monster.log').should include('New monster created!')
+  end
 end 
