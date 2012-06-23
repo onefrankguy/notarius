4448df0da14c5ce68a3d33d617a6907008820bdd
elimossinary <me@frankmitchell.org>
2012-06-23 19:30:42 -0400

    We can now log to files.

--- lib/notarius.rb
+++ lib/notarius.rb
@@ -1,5 +1,27 @@
+require 'logger'
+
 module Notarius
-  def self.configure name
-    self.const_set name, Module.new
+  def self.configure name, &block
+    config = LogConfig.new
+    config.instance_eval(&block) if block_given?
+    mod = Module.new do
+      @@log = Logger.new(config.file.path)
+      @@log.level = Logger::INFO
+      def log
+        @@log
+      end
+    end
+    self.const_set name, mod
+  end
+
+  class LogConfig 
+    attr_accessor :file
+    def initialize
+      @file = FileConfig.new
+    end
+  end
+
+  class FileConfig
+    attr_accessor :path
   end
 end
--- spec/notarius_spec.rb
+++ spec/notarius_spec.rb
@@ -5,4 +5,19 @@ describe Notarius do
     Notarius.configure 'BIG'
     Notarius::BIG.class.should == Module
   end
+
+  it 'can log to a file' do
+    FileUtils.rm 'player.log' if File.exists? 'player.log'
+    Notarius.configure 'BIG' do |l|
+      l.file.path = 'player.log'
+    end
+    class Player
+      include Notarius::BIG
+      def initialize
+        log.info 'New player created!'
+      end
+    end
+    Player.new
+    File.read('player.log').should include('New player created!')
+  end
 end 
