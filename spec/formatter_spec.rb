9dc37228747cf0fa0d133c8b74ac09bbdc6e6485
elimossinary <me@frankmitchell.org>
2012-06-25 20:39:07 -0400

    Simplified spec for Formatter class.

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -1,23 +1,11 @@
 require 'notarius/formatter'
 
 describe Notarius::Formatter do
-  it 'converts whitespace to spaces' do
-    formatter = Notarius::Formatter.new
-
-    message = "\tMessage\t\twith\ttabs\t\t"
-    message = formatter.call(nil, nil, nil, message) 
-    message.should == 'Message with tabs'
-
-    message = "\nMessage\r\rwith\rcarriage\rreturns\r\r"
-    message = formatter.call(nil, nil, nil, message) 
-    message.should == 'Message with carriage returns'
-
-    message = "\nMessage\n\nwith\nnew\nlines\n\n"
-    message = formatter.call(nil, nil, nil, message) 
-    message.should == 'Message with new lines'
-
-    message = " Message  with extra spaces  "
-    message = formatter.call(nil, nil, nil, message) 
-    message.should == 'Message with extra spaces'
+  describe 'call' do
+    it 'converts whitespace to spaces' do
+      message = "\sMessage\r\nwith\twhitespace  "
+      message = Notarius::Formatter.new.call(nil, nil, nil, message)
+      message.should == 'Message with whitespace'
+    end
   end
 end
