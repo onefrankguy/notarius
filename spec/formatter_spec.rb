546025ed014e7728bdea4b23598d4be5fa0d620c
elimossinary <me@frankmitchell.org>
2012-06-25 20:52:51 -0400

    Moved duplicate code in Formatter spec into let.

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -2,15 +2,17 @@ require 'notarius/formatter'
 
 describe Notarius::Formatter do
   describe 'call' do
+    let(:formatter) { Notarius::Formatter.new }
+
     it 'converts whitespace to spaces' do
       message = "\sMessage\r\nwith\twhitespace  "
-      message = Notarius::Formatter.new.call(nil, nil, nil, message)
+      message = formatter.call(nil, nil, nil, message)
       message.should == 'Message with whitespace'
     end
 
     it 'formats timestamps as ISO 8601' do
       timestamp = Time.parse('2012-06-25 20:41:30 -0400')
-      message = Notarius::Formatter.new.call(nil, timestamp, nil, nil)
+      message = formatter.call(nil, timestamp, nil, nil)
       message.should == '[2012-06-26T00:41:30Z]'
     end
   end
