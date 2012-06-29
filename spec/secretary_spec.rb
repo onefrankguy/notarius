56c4fd0ea87f2ec0fa51504b4ee569c92ca92d78
elimossinary <me@frankmitchell.org>
2012-06-29 07:22:56 -0400

    Simplified Secretary specification.

--- spec/secretary_spec.rb
+++ spec/secretary_spec.rb
@@ -2,26 +2,31 @@ require 'notarius/secretary'
 require 'stringio'
 
 describe Notarius::Secretary do
+  let(:logger) { StringIO.new }
+  let(:secretary) do
+    c = Notarius::Config.new
+    c.file = logger
+    s = Notarius::Secretary.new
+    s.configure c
+    s
+  end
+
+  before :each do
+    logger.truncate 0
+    logger.rewind
+  end
+
   it 'can log info messages' do
-    logger = StringIO.new
-    secretary = Notarius::Secretary.new
-    secretary.add 'logger', logger
     secretary.info 'info message'
     logger.string.should match(/^INFO \[[^\]]+\] info message\n$/)
   end
 
   it 'can log warning messages' do
-    logger = StringIO.new
-    secretary = Notarius::Secretary.new
-    secretary.add 'logger', logger
     secretary.warn 'warning message'
     logger.string.should match(/^WARN \[[^\]]+\] warning message\n$/)
   end
 
   it 'can log error messages' do
-    logger = StringIO.new
-    secretary = Notarius::Secretary.new
-    secretary.add 'logger', logger
     secretary.error 'error message'
     logger.string.should match(/^ERROR \[[^\]]+\] error message\n$/)
   end
