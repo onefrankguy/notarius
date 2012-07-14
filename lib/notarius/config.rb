9cf0368cdb75248498f1cdac8c65e81ccdb8d162
elimossinary <me@frankmitchell.org>
2012-07-14 14:22:41 -0400

    Adds documentation about logging configuration options.

--- lib/notarius/config.rb
+++ lib/notarius/config.rb
@@ -1,6 +1,13 @@
 module Notarius
   class Config
+    ##
+    # Turn console logging on or off. Default is off.
+    # @return [Boolean] whether console logging is enabled
     attr_accessor :console
+
+    ##
+    # Turn file logging on or off. Default is off.
+    # @return [String, nil] path to the file, +nil+ if off
     attr_accessor :file
   end
 end
